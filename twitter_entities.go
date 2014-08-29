0117a303aa640733c0992b124b7944a6e7dd64f2
Jake Januzelli <januzellij@gmail.com>
2014-08-29 15:02:55 -0400

    MOAR JSON

--- twitter_entities.go
+++ twitter_entities.go
@@ -19,24 +19,24 @@ type Entities struct {
 		Id_str      string `json:",omitempty"`
 	}
 	Media []struct {
-		Id              int64
-		Id_str          string
-		Media_url       string
-		Media_url_https string
-		Url             string
-		Display_url     string
-		Expanded_url    string
-		Sizes           MediaSizes
-		Type            string
-		Indices         []int
+		Id              int64      `json:",omitempty"`
+		Id_str          string     `json:",omitempty"`
+		Media_url       string     `json:",omitempty"`
+		Media_url_https string     `json:",omitempty"`
+		Url             string     `json:",omitempty"`
+		Display_url     string     `json:",omitempty"`
+		Expanded_url    string     `json:",omitempty"`
+		Sizes           MediaSizes `json:",omitempty"`
+		Type            string     `json:",omitempty"`
+		Indices         []int      `json:",omitempty"`
 	}
 }
 
 type MediaSizes struct {
-	Medium MediaSize
-	Thumb  MediaSize
-	Small  MediaSize
-	Large  MediaSize
+	Medium MediaSize `json:",omitempty"`
+	Thumb  MediaSize `json:",omitempty"`
+	Small  MediaSize `json:",omitempty"`
+	Large  MediaSize `json:",omitempty"`
 }
 
 type MediaSize struct {
