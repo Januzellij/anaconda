e387080c71c3361206ea9ac24b8a744b418de80a
Jake Januzelli <januzellij@gmail.com>
2014-08-29 14:01:36 -0400

    made archive JSON smaller

--- archive_twitter_entities.go
+++ archive_twitter_entities.go
@@ -2,32 +2,32 @@ package anaconda
 
 type ArchiveEntities struct {
 	Hashtags []struct {
-		Indices []int
-		Text    string
+		Indices []int  "json:,omitempty"
+		Text    string "json:,omitempty"
 	}
 	Urls []struct {
-		Indices      []int
-		Url          string
-		Display_url  string
-		Expanded_url string
+		Indices      []int  "json:,omitempty"
+		Url          string "json:,omitempty"
+		Display_url  string "json:,omitempty"
+		Expanded_url string "json:,omitempty"
 	}
 	User_mentions []struct {
-		Name        string
-		Indices     []int
-		Screen_name string
-		Id          int64
-		Id_str      string
+		Name        string "json:,omitempty"
+		Indices     []int  "json:,omitempty"
+		Screen_name string "json:,omitempty"
+		Id          int64  "json:,omitempty"
+		Id_str      string "json:,omitempty"
 	}
 	Media []struct {
-		Id              int64
-		Id_str          string
-		Media_url       string
-		Media_url_https string
-		Url             string
-		Display_url     string
-		Expanded_url    string
-		Sizes           []MediaSize
-		Type            string
-		Indices         []int
+		Id              int64       "json:,omitempty"
+		Id_str          string      "json:,omitempty"
+		Media_url       string      "json:,omitempty"
+		Media_url_https string      "json:,omitempty"
+		Url             string      "json:,omitempty"
+		Display_url     string      "json:,omitempty"
+		Expanded_url    string      "json:,omitempty"
+		Sizes           []MediaSize "json:,omitempty"
+		Type            string      "json:,omitempty"
+		Indices         []int       "json:,omitempty"
 	}
 }
