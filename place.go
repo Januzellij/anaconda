3e99a84b3e9a92ee2d17343632d356885328985b
Jake Januzelli <januzellij@gmail.com>
2014-08-29 17:09:27 -0400

    Revert "Revert "think I did all the JSON""
    This reverts commit 7a2d90aa69248f494700d86847204340da3990a2.

--- place.go
+++ place.go
@@ -1,35 +1,35 @@
 package anaconda
 
 type Place struct {
-	Attributes  map[string]string `json:"attributes"`
+	Attributes  map[string]string `json:"attributes,omitempty"`
 	BoundingBox struct {
-		Coordinates [][][]float64 `json:"coordinates"`
-		Type        string        `json:"type"`
-	} `json:"bounding_box"`
+		Coordinates [][][]float64 `json:"coordinates,omitempty"`
+		Type        string        `json:"type,omitempty"`
+	} `json:"bounding_box,omitempty"`
 	ContainedWithin []struct {
-		Attributes  map[string]string `json:"attributes"`
+		Attributes  map[string]string `json:"attributes,omitempty"`
 		BoundingBox struct {
-			Coordinates [][][]float64 `json:"coordinates"`
-			Type        string        `json:"type"`
-		} `json:"bounding_box"`
-		Country     string `json:"country"`
-		CountryCode string `json:"country_code"`
-		FullName    string `json:"full_name"`
-		ID          string `json:"id"`
-		Name        string `json:"name"`
-		PlaceType   string `json:"place_type"`
-		URL         string `json:"url"`
-	} `json:"contained_within"`
-	Country     string `json:"country"`
-	CountryCode string `json:"country_code"`
-	FullName    string `json:"full_name"`
+			Coordinates [][][]float64 `json:"coordinates,omitempty"`
+			Type        string        `json:"type,omitempty"`
+		} `json:"bounding_box,omitempty"`
+		Country     string `json:"country,omitempty"`
+		CountryCode string `json:"country_code,omitempty"`
+		FullName    string `json:"full_name,omitempty"`
+		ID          string `json:"id,omitempty"`
+		Name        string `json:"name,omitempty"`
+		PlaceType   string `json:"place_type,omitempty"`
+		URL         string `json:"url,omitempty"`
+	} `json:"contained_within,omitempty"`
+	Country     string `json:"country,omitempty"`
+	CountryCode string `json:"country_code,omitempty"`
+	FullName    string `json:"full_name,omitempty"`
 	Geometry    struct {
-		Coordinates [][][]float64 `json:"coordinates"`
-		Type        string        `json:"type"`
-	} `json:"geometry"`
-	ID        string   `json:"id"`
-	Name      string   `json:"name"`
-	PlaceType string   `json:"place_type"`
-	Polylines []string `json:"polylines"`
-	URL       string   `json:"url"`
+		Coordinates [][][]float64 `json:"coordinates,omitempty"`
+		Type        string        `json:"type,omitempty"`
+	} `json:"geometry,omitempty"`
+	ID        string   `json:"id,omitempty"`
+	Name      string   `json:"name,omitempty"`
+	PlaceType string   `json:"place_type,omitempty"`
+	Polylines []string `json:"polylines,omitempty"`
+	URL       string   `json:"url,omitempty"`
 }
