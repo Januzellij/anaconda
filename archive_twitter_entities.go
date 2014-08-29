7643d854d01dd1f487f0b3cdb5b06cc885c12922
Jake Januzelli <januzellij@gmail.com>
2014-08-28 20:52:36 -0400

    supports *both* archive and API JSON

--- archive_twitter_entities.go
+++ archive_twitter_entities.go
@@ -1,6 +1,6 @@
 package anaconda
 
-type Entities struct {
+type ArchiveEntities struct {
 	Hashtags []struct {
 		Indices []int
 		Text    string
@@ -26,17 +26,10 @@ type Entities struct {
 		Url             string
 		Display_url     string
 		Expanded_url    string
-		Sizes           MediaSizes
+		Sizes           []MediaSize
 		Type            string
 		Indices         []int
 	}
-}
-
-type MediaSizes struct {
-	Medium MediaSize
-	Thumb  MediaSize
-	Small  MediaSize
-	Large  MediaSize
 }
 
 type MediaSize struct {
