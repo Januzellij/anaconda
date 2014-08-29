6d74476de5d3d34654dccbff8a32a6cd3edfc544
Jake Januzelli <januzellij@gmail.com>
2014-08-29 14:10:51 -0400

    forgot backticks

--- archive_twitter_entities.go
+++ archive_twitter_entities.go
@@ -2,32 +2,32 @@ package anaconda
 
 type ArchiveEntities struct {
 	Hashtags []struct {
-		Indices []int  "json:,omitempty"
-		Text    string "json:,omitempty"
+		Indices []int  `"json:,omitempty"`
+		Text    string `"json:,omitempty"`
 	}
 	Urls []struct {
-		Indices      []int  "json:,omitempty"
-		Url          string "json:,omitempty"
-		Display_url  string "json:,omitempty"
-		Expanded_url string "json:,omitempty"
+		Indices      []int  `"json:,omitempty"`
+		Url          string `"json:,omitempty"`
+		Display_url  string `"json:,omitempty"`
+		Expanded_url string `"json:,omitempty"`
 	}
 	User_mentions []struct {
-		Name        string "json:,omitempty"
-		Indices     []int  "json:,omitempty"
-		Screen_name string "json:,omitempty"
-		Id          int64  "json:,omitempty"
-		Id_str      string "json:,omitempty"
+		Name        string `"json:,omitempty"`
+		Indices     []int  `"json:,omitempty"`
+		Screen_name string `"json:,omitempty"`
+		Id          int64  `"json:,omitempty"`
+		Id_str      string `"json:,omitempty"`
 	}
 	Media []struct {
-		Id              int64       "json:,omitempty"
-		Id_str          string      "json:,omitempty"
-		Media_url       string      "json:,omitempty"
-		Media_url_https string      "json:,omitempty"
-		Url             string      "json:,omitempty"
-		Display_url     string      "json:,omitempty"
-		Expanded_url    string      "json:,omitempty"
-		Sizes           []MediaSize "json:,omitempty"
-		Type            string      "json:,omitempty"
-		Indices         []int       "json:,omitempty"
+		Id              int64       `"json:,omitempty"`
+		Id_str          string      `"json:,omitempty"`
+		Media_url       string      `"json:,omitempty"`
+		Media_url_https string      `"json:,omitempty"`
+		Url             string      `"json:,omitempty"`
+		Display_url     string      `"json:,omitempty"`
+		Expanded_url    string      `"json:,omitempty"`
+		Sizes           []MediaSize `"json:,omitempty"`
+		Type            string      `"json:,omitempty"`
+		Indices         []int       `"json:,omitempty"`
 	}
 }
