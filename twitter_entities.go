d7c79f294eb869c44549b4a7efc7006296c149a1
Jake Januzelli <januzellij@gmail.com>
2014-08-29 22:47:31 -0400

    added son tags to entities

--- twitter_entities.go
+++ twitter_entities.go
@@ -2,45 +2,45 @@ package anaconda
 
 type Entities struct {
 	Hashtags []struct {
-		Indices []int
-		Text    string
+		Indices []int  `json:"indices"`
+		Text    string `json:"text"`
 	}
 	Urls []struct {
-		Indices      []int
-		Url          string
-		Display_url  string
-		Expanded_url string
+		Indices      []int  `json:"indices"`
+		Url          string `json:"url"`
+		Display_url  string `json:"display_url"`
+		Expanded_url string `json:"expanded_url"`
 	}
 	User_mentions []struct {
-		Name        string
-		Indices     []int
-		Screen_name string
-		Id          int64
-		Id_str      string
+		Name        string `json:"name"`
+		Indices     []int  `json:"indices"`
+		Screen_name string `json:"screen_name"`
+		Id          int64  `json:"id"`
+		Id_str      string `json:"id_str"`
 	}
 	Media []struct {
-		Id              int64
-		Id_str          string
-		Media_url       string
-		Media_url_https string
-		Url             string
-		Display_url     string
-		Expanded_url    string
-		Sizes           MediaSizes
-		Type            string
-		Indices         []int
+		Id              int64      `json:"id"`
+		Id_str          string     `json:"id_str"`
+		Media_url       string     `json:"media_url"`
+		Media_url_https string     `json:"media_url_https"`
+		Url             string     `json:"url"`
+		Display_url     string     `json:"display_url"`
+		Expanded_url    string     `json:"expanded_url"`
+		Sizes           MediaSizes `json:"sizes"`
+		Type            string     `json:"type"`
+		Indices         []int      `json:"indices"`
 	}
 }
 
 type MediaSizes struct {
-	Medium MediaSize
-	Thumb  MediaSize
-	Small  MediaSize
-	Large  MediaSize
+	Medium MediaSize `json:"medium"`
+	Thumb  MediaSize `json:"thumb"`
+	Small  MediaSize `json:"small"`
+	Large  MediaSize `json:"large"`
 }
 
 type MediaSize struct {
-	W      int
-	H      int
-	Resize string
+	W      int    `json:"w"`
+	H      int    `json:"h"`
+	Resize string `json:"resize"`
 }
